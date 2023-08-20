9feddd69e72b7afb8558564615b0a392fb8e5e35
Francis Ofori Anane <ofori.anane@gmail.com>
2023-08-20 03:21:19 +0000

    Add files

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,11 @@
+# set ulimit to handle high amount of requests
+exec {'set_ulimit':
+  provider => shell,
+  command  => 'sudo sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 4097\"/" /etc/default/nginx',
+  before   => Exec['server_restart'],
+}
+
+exec {'server_restart':
+  provider => shell,
+  command  => 'sudo service nginx restart',
+}
---
+++ 0x1B-web_stack_debugging_4/README.md
@@ -0,0 +1,1 @@
+# My school project work 0x1B. Web stack debugging #4
